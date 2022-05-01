aac9a1e91071311b49dba355fcecf8f5565a3d25
JaredKIZA <104637618+JaredKIZA@users.noreply.github.com>
2022-05-01 17:43:01 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Jared KIZA Palmer</title>
+</head>
+<body>
+	<h1>Bienvenue dans ma page de blog personnel</h1> <br>
+		<img src="jared.jpg">
+		<hr>Copyright @2022 - KIZA DIMANDJA
+</body>
+</html>
