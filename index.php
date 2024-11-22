9287cbf5d2e698d5e582c61dca33cfbaca034c97
KIZA DIM Jacques <104637618+JaredKIZA@users.noreply.github.com>
2024-11-22 08:40:15 +0100

    index.php

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Jared KIZA Palmer</title>
+	<title>Jacques KIZA DIMANDJA</title>
 </head>
 <body>
 	<h1>Bienvenue dans ma page de blog personnel</h1> <br>
 		<img src="jared.jpg">
-		<hr>Copyright @2022 - KIZA DIMANDJA
+		<hr>Copyright @2024 - Jacques KIZA DIMANDJA
 </body>
 </html>
